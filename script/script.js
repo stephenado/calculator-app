8bcd66d11177c030825e2bb8422fa75205e0375c
[stephenado] <[stephenado100@gmail.com]>
2019-10-15 12:39:26 -0700

    i added some functions for addition

---
+++ script/script.js
@@ -0,0 +1,23 @@
+function btnplus(){
+    document.calculator.display.value+="+";
+}
+
+function btnsub(){
+    document.calculator.display.value+="-";
+}
+
+function btnmult(){
+    document.calculator.display.value+="*";
+}
+
+function btnmod(){
+document.calculator.display.value+="%";
+}
+
+function btndot(){
+document.calculator.display.value+=".";
+}
+
+function btndiv(){
+document.calculator.display.value+="/";
+}
